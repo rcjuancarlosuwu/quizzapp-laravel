96dd912a385ce45be3a0f9dd87c0ed2c65cd8ff0
Juan <rcjuancarlos01@gmail.com>
2021-01-25 08:20:45 -0500

    fastgit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Fruitcake\Cors\HandleCors::class,
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
